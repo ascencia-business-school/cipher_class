6c846fb79b9e76414ce44d9e5de503cb8db42faf
Victor Chidubem Mazeli <52371324+victormazeli@users.noreply.github.com>
2024-11-23 11:36:39 +0000

    update on comments

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ def caesar_cipher_encrypt(text, shift):
     Encrypts the given text using the Caesar cipher technique.
     """
     encrypted_text = []
-
+    #Loop through each characters in the text
     for char in text:
+        # Check if the character is an alpher numeric character
         if char.isalpha(): 
+            # Get the integer value for the ASCII unicode of the Uppercase "A" and "a" only if the character is either uppercase or lowercase
             start = ord('A') if char.isupper() else ord('a')
             new_char = chr((ord(char) - start + shift) % 26 + start)
             encrypted_text.append(new_char)
@@ -21,4 +23,4 @@ shift = 3
 encrypted = caesar_cipher_encrypt(text, shift)
 print("Encrypted Text:", encrypted)
 re_encrypted = caesar_cipher_encrypt(encrypted, -3)
-print("RE-Encrypted Text:", re_encrypted)
+print("DE-Encrypted Text:", re_encrypted)
